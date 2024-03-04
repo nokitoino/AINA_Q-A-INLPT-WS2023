0882e548afab331a0e997558ecc6cf106a5503b6
Ibinmbiju <ibinmbiju@gmail.com.com>
2024-03-04 06:58:35 +0100

    Added DocStrings

--- Scraper/PubMedScraper.py
+++ Scraper/PubMedScraper.py
@@ -6,19 +6,36 @@ from datetime import datetime, timedelta
 # Set your email address for the Entrez API
 Entrez.email = "_"
 
-
 def fetch_details(id_list):
+    """
+    Fetch details for a list of PubMed article IDs.
+
+    Parameters:
+        id_list (list): List of PubMed article IDs.
+
+    Returns:
+        dict: Details of the fetched articles.
+    """
     ids = ','.join(id_list)
     handle = Entrez.efetch(db='pubmed', id=ids, retmode='xml')
     papers = Entrez.read(handle)
     return papers
 
 def generate_date_intervals(start_date, end_date, days):
-    date_format = "%Y/%m/%d"
+    """
+    Generate date intervals between a start and end date.
 
+    Parameters:
+        start_date (str): Start date in the format 'YYYY/MM/DD'.
+        end_date (str): End date in the format 'YYYY/MM/DD'.
+        days (int): Number of days for each interval.
+
+    Returns:
+        list: List of date intervals.
+    """
+    date_format = "%Y/%m/%d"
     start_datetime = datetime.strptime(start_date, date_format)
     end_datetime = datetime.strptime(end_date, date_format)
-
     current_datetime = start_datetime
     date_intervals = []
 
@@ -26,9 +43,7 @@ def generate_date_intervals(start_date, end_date, days):
         date_intervals.append(current_datetime.strftime(date_format))
         current_datetime += timedelta(days=days)
 
-    # Add the end date to ensure the last interval covers the remaining days
     date_intervals.append(end_datetime.strftime(date_format))
-
     return date_intervals
 
 # Your search query parameters
@@ -54,7 +69,7 @@ for interval_start, interval_end in zip(intervals[:-1], intervals[1:]):
 
     # Extract the list of IDs
     studies_id_list = search_results['IdList']
-    print(interval_start,interval_end,len(studies_id_list))
+
     # Fetch details in chunks
     for chunk_i in range(0, len(studies_id_list), chunk_size):
         chunk = studies_id_list[chunk_i:chunk_i + chunk_size]
@@ -82,60 +97,67 @@ for interval_start, interval_end in zip(intervals[:-1], intervals[1:]):
 
             try:
                 data["title"]["full_text"] = paper['MedlineCitation']['Article']['ArticleTitle'].lower()
-                data["title"]["tokens"] = data["title"]["full_text"].split()  # Use specil Tokenizer here, for removing stopwords ect.
+                data["title"]["tokens"] = data["title"]["full_text"].split()
                 for author in paper['MedlineCitation']['Article']['AuthorList']:
                     author_name = f"{author.get('LastName', '')}, {author.get('ForeName', '')}"
                     data["authors"].append(author_name)
             except:
                 ...
+
             try:
                 data["abstract"]["full_text"] = paper['MedlineCitation']['Article']['Abstract']['AbstractText'][0].lower()
                 data["abstract"]["tokens"] = data["abstract"]["full_text"].split()
             except:
                 data["abstract"]["full_text"] = ''
+
             data["journal"] = paper['MedlineCitation']['Article']['Journal']['Title'].lower()
             data["language"] = paper['MedlineCitation']['Article']['Language'][0].lower()
+
             try:
                 data["year"] = paper['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate']['Year'].lower()
             except:
                 data["year"] = ""
+
             try:
                 data["month"] = paper['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate']['Month'].lower()
             except:
                 data["month"] = ""
 
-            # Extracting Keywords
             keywords = []
+
             if 'KeywordList' in paper['MedlineCitation']:
                 for keyword_list in paper['MedlineCitation']['KeywordList']:
                     keywords.append(keyword_list)
+
             data["keywords"] = keywords
 
-            # Extracting References
             references = []
+
             if 'PubmedData' in paper and 'ReferenceList' in paper['PubmedData']:
                 for reference in paper['PubmedData']['ReferenceList']:
                     for citation in reference['Reference']:
-                        references.append(citation['Citation'])  # or by ArticleIdList we can access the DOI directly
+                        references.append(citation['Citation'])
+
             data["references"] = references
 
-            # Extracting Affiliations
             affiliations = []
+
             if 'AuthorList' in paper['MedlineCitation']['Article']:
                 author_list = paper['MedlineCitation']['Article']['AuthorList']
+
                 for author_info in author_list:
                     if 'AffiliationInfo' in author_info:
                         for affiliation_info in author_info['AffiliationInfo']:
                             affiliation = affiliation_info.get('Affiliation', '')
                             affiliations.append(affiliation)
-            else:
-                ...
-                #print(f"No affiliation information for paper {i}")
+                    else:
+                        ...
+                        # print(f"No affiliation information for paper {i}")
 
             data["affiliations"] = affiliations
 
-            # Identifiers (e.g., DOI, PMID)
             identifiers = {}
+
             if 'PubmedData' in paper and 'ArticleIdList' in paper['PubmedData']:
                 for identifier in paper['PubmedData']['ArticleIdList']:
                     data["identifiers"][identifier.attributes['IdType']] = str(identifier)
--- Text_generation_using_Hybrid_search_diff_LLMs_modifies.py
+++ Text_generation_using_Hybrid_search_diff_LLMs_modifies.py
@@ -1,5 +1,5 @@
 # Import necessary packages
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline, set_seed,AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline, set_seed, AutoModelForCausalLM
 import torch
 from langchain_community.llms import HuggingFacePipeline
 from langchain_openai import OpenAI
@@ -9,8 +9,6 @@ from Hybrid_search import HybridSearch
 from dotenv import load_dotenv
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
-
-
 
 import os
 load_dotenv() 
@@ -32,11 +30,29 @@ HF_TOKEN = "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"
 
 # Helper functions
 def get_relevant_context(question):
+    """
+    Get relevant context from HybridSearch for a given question.
+
+    Args:
+        question (str): User's question.
+
+    Returns:
+        str: Relevant context.
+    """
     docs = hybrid_search_setup.get_relevant_documents(question)
     context = " ".join([doc.get('page_content', '') for doc in docs[:2]])
     return context
 
 def get_documents_and_metadata(question):
+    """
+    Get relevant documents and metadata from HybridSearch for a given question.
+
+    Args:
+        question (str): User's question.
+
+    Returns:
+        tuple: Tuple containing context and metadata string.
+    """
     documents = hybrid_search_setup.get_relevant_documents(question)
     # Adjusted to use .get() for safer key access
     context = "\n".join([doc.get('abstract', '') for doc in documents[:2]])
@@ -49,6 +65,15 @@ def prepare_prompt(question, context, metadata_str, answer=None):
     """
     Prepare the prompt by including context, metadata, and the question for the LLM,
     and handle the case when the answer is "I don't know."
+
+    Args:
+        question (str): User's question.
+        context (str): Relevant context.
+        metadata_str (str): Metadata string.
+        answer (str): Optional answer.
+
+    Returns:
+        str: Prepared prompt.
     """
     prompt = f"""
     You are a helpful AI Assistant that follows instructions extremely well.
@@ -71,140 +96,20 @@ def prepare_prompt(question, context, metadata_str, answer=None):
 
 # Method 1: Using PubMedBERT for Question Answering
 def method_1_pubmed_bert(question):
+    """
+    Method 1: Using PubMedBERT for Question Answering.
+
+    Args:
+        question (str): User's question.
+    """
     model_id = "gerardozq/biobert_v1.1_pubmed-finetuned-squad"
     tokenizer = AutoTokenizer.from_pretrained(model_id, use_auth_token=HF_TOKEN)
     model = AutoModelForQuestionAnswering.from_pretrained(model_id, use_auth_token=HF_TOKEN)
-    qa_pipeline = pipeline('question-answering', model=model, tokenizer=tokenizer,max_seq_len=1024)
-    context,metadata_str = get_documents_and_metadata(question)
-    response = qa_pipeline(question=question,context=context)
+    qa_pipeline = pipeline('question-answering', model=model, tokenizer=tokenizer, max_seq_len=1024)
+    context, metadata_str = get_documents_and_metadata(question)
+    response = qa_pipeline(question=question, context=context)
     #print(response)
     if response['score'] < 0.05:
         response['answer'] = 'I dont know'
     print(response)  
-    #prompt = prepare_prompt(question,context,metadata_str)
-    #print(type(response))
-    #print(response)
-    #print(f"The answer:{response['answer']}")
-
-
-# Method 2: Standard LLM OpenAI
-def method_2_openai_llm(question):
-    qa_chain = RetrievalQA.from_chain_type(llm=OpenAI(), retriever=hybrid_search_setup.ensemble_retriever, return_source_documents=True)
-    llm_response = qa_chain(question)
-    #print(llm_response)
-    context = ''
-    metadatas = []
-    for doc in llm_response['source_documents']:
-        page_content = doc.page_content
-        metadata = metadatas.append(doc.metadata)
-        context+=page_content
-    combined_metadata = "\n".join([f"Title: {m.get('title', '')}\nKeywords: {', '.join(m.get('keywords', []))}" for m in metadatas])
-    if "I'm sorry" in llm_response["result"] or  "there is no mention" in llm_response["result"] or "I don't know" in llm_response["result"] or "I do not" in llm_response["result"]:
-    # Make CONTEXT and METADATA empty
-        llm_response["source_documents"] = ""
-        llm_response["result"] = "I don't know"
-        prompt = prepare_prompt(question,context,combined_metadata)
-    #print(prompt)
-        print(f"The generated solution: {llm_response}")
-    else:
-        prompt = prepare_prompt(question,context,combined_metadata)
-        print(prompt)
-        print(f"The generated response: {llm_response['result']}")
-                  
-# helper function for huggingfacehub  
-def response_to_dict_v2_v2(generated_response):
-    # Initialize an empty dictionary to store the extracted information
-    response_dict = {}
-
-    section_labels = ["QUESTION:", "CONTEXT:", "METADATA:", "ANSWER:"]
-
-    # Initialize an empty list to keep track of found labels
-    found_labels = []
-
-    # Identify all occurrences of section labels and their indexes
-    for label in section_labels:
-        index = generated_response.find(label)
-        if index != -1:  # If label is found
-            found_labels.append((label, index))
-
-    # Sort found labels by their index to maintain the correct order
-    found_labels.sort(key=lambda x: x[1])
-
-    # Iterate over found labels and extract content between them
-    for i, (label, index) in enumerate(found_labels):
-        start_index = index + len(label)
-        end_index = None if i + 1 == len(found_labels) else found_labels[i + 1][1]
-        content = generated_response[start_index:end_index].strip() if end_index is not None else generated_response[start_index:].strip()
-        response_dict[label.strip(":")] = content
-
-    # Ensure all section keys exist
-    for section in section_labels:
-        section_key = section.strip(":")
-        if section_key not in response_dict:
-            response_dict[section_key] = ""  # Set empty string for missing sections
-
-    return response_dict
-
-# Method 3: Using HuggingFaceHub
-def method_3_huggingface_hub(question):
-    model_id = 'HuggingFaceH4/zephyr-7b-beta'
-    llm = HuggingFaceHub(repo_id=model_id, model_kwargs={"temperature": 0.3, "max_new_tokens": 500}, huggingfacehub_api_token=HF_TOKEN)
-    context, metadata_str = get_documents_and_metadata(question)
-    prompt = prepare_prompt(question, context, metadata_str)
-    generated_text = llm.invoke(prompt)
-    response_dict = response_to_dict_v2_v2(generated_text)
-
-# Checking if ANSWER contains "I don't know" or is blank
-    if "I don't know" in response_dict["ANSWER"] or not response_dict["ANSWER"].strip():
-    # Make CONTEXT and METADATA empty
-        response_dict["CONTEXT"] = ""
-        response_dict["METADATA"] = ""
-        response_dict["ANSWER"] = "I don't know"
-    print(response_dict)
-
-# Method 4: Using Medalpaca from Hugging Face as LLMs
-def method_4_medalpaca(question, model_dir):
-    model_id = "medalpaca/medalpaca-7b"
-    
-    # Define paths for the model and tokenizer
-    model_file_path = '/Users/anureddy/Desktop/SEM03/Natural_Language_Processing_with_Transformers/Project_QA/_Q-A-INLPT-WS2023/DB/Medalpeca/medalpaca_model'
-    tokenizer_file_path = '/Users/anureddy/Desktop/SEM03/Natural_Language_Processing_with_Transformers/Project_QA/_Q-A-INLPT-WS2023/DB/Medalpeca/medalpaca_tokenizer'
-
-    # Check if the model and tokenizer are already downloaded
-    if not os.path.exists(model_file_path) or not os.path.exists(tokenizer_file_path):
-        print("Downloading model and tokenizer...")
-        tokenizer = AutoTokenizer.from_pretrained(model_id)
-        model = AutoModelForCausalLM.from_pretrained(model_id)
-        # Save the model and tokenizer locally
-        tokenizer.save_pretrained(model_dir)
-        model.save_pretrained(model_dir)
-    else:
-        print("Loading model and tokenizer from local directory...")
-        tokenizer = AutoTokenizer.from_pretrained(tokenizer_file_path)
-        model = AutoModelForCausalLM.from_pretrained(model_file_path)
-    
-    # Generate text
-    pl_loaded = pipeline("text-generation", model=model, tokenizer=tokenizer)
-    context,metadata_str = get_documents_and_metadata(question)
-    inputs = f"Context: {context}\n\nQuestion: {question}\n\nAnswer:"# Make sure this function is defined
-    try:
-        
-        prompt = prepare_prompt(question,context,metadata_str)
-        response = pl_loaded(inputs,max_length=1024,num_return_sequences=1, do_sample=True,truncation=True)
-        print("Generated response:\n", response[0]['generated_text'])
-    except Exception as e:
-        print(f"An error occurred during text generation: {str(e)}")
-
-
-
-# Example usage
-if __name__ == "__main__":
-    model_dir = "/Users/anureddy/Desktop/SEM03/Natural_Language_Processing_with_Transformers/Project_QA/_Q-A-INLPT-WS2023/Medalpeca"
-    os.makedirs(model_dir, exist_ok=True)
-    question = 'Are keratin 8 Y54H and G62C mutations associated with inflammatory bowel disease?'
-    #question = "Why did the researchers develop text classifiers for detecting invasive fungal diseases from free-text radiology reports?"
-    #question = 'what is anu'
-    method_1_pubmed_bert(question)
-    #method_2_openai_llm(question)
-    #method_3_huggingface_hub(question)
-    #method_4_medalpaca(question,model_dir)
+    #prompt = prepare_prompt(question, context, metadata_str)
