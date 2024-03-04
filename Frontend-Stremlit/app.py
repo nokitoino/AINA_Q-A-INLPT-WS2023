7886cec3cbfa4d326c53a20f2771393391064fe5
Ibinmbiju <ibinmbiju@gmail.com.com>
2024-03-04 06:39:29 +0100

    Streamlit implementation

---
+++ Frontend-Stremlit/app.py
@@ -0,0 +1,151 @@
+import json
+import random
+import openai
+import streamlit as st
+from langchain import hub
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from Hybrid_search import HybridSearch
+import warnings
+
+warnings.filterwarnings("ignore")
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+
+st.set_page_config(page_title="Conversational Agent", layout="wide")
+
+@st.experimental_singleton
+def initialize_components():
+    """
+    Initialize global components only once.
+
+    Returns:
+        Tuple: Tuple containing HybridSearch instance, RAG prompt, ChatOpenAI instance, and ContextualCompressionRetriever instance.
+    """
+    data_path = './papers_latest.json'
+    hs = HybridSearch(data_path)
+    data = hs.load_data()
+    docs = hs.transform_data_to_documents(data)
+    bm25_retriever = hs.initialize_bm25_retriever(docs)
+    chroma_vectorstore = hs.process_documents_with_chroma(docs)
+    hs.create_ensemble_retriever(bm25_retriever, chroma_vectorstore)
+    
+    prompt = hub.pull("rlm/rag-prompt")
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    compressor = LLMChainExtractor.from_llm(llm=llm)
+    compression_retriever = ContextualCompressionRetriever(
+        base_compressor=compressor, base_retriever=hs.ensemble_retriever
+    )
+    
+    return hs, prompt, llm, compression_retriever
+
+def display_conversation_interface(compression_retriever, prompt, llm):
+    """
+    Display the main conversation interface.
+
+    Args:
+        compression_retriever (ContextualCompressionRetriever): Instance of the compression retriever.
+        prompt (str): RAG prompt.
+        llm (ChatOpenAI): Instance of ChatOpenAI model.
+    """
+    st.title("Your PubMed Bot")
+
+    if 'query' not in st.session_state:
+        st.session_state['query'] = ""
+    
+    query = st.text_input("Ask me anything about PubMed:", value=st.session_state['query'], key="query_input")
+
+    if st.button('Ask') or query:
+        st.session_state['query'] = query
+        generate_response_and_follow_ups(query, compression_retriever, prompt, llm)
+
+def generate_response_and_follow_ups(query, compression_retriever, prompt, llm):
+    """
+    Generate response and follow-up questions based on the user query.
+
+    Args:
+        query (str): User's query.
+        compression_retriever (ContextualCompressionRetriever): Instance of the compression retriever.
+        prompt (str): RAG prompt.
+        llm (ChatOpenAI): Instance of ChatOpenAI model.
+    """
+    compressed_docs = compression_retriever.get_relevant_documents(query)
+    rag_chain_compressor = (
+        {"context": compression_retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    generated_response = rag_chain_compressor.invoke(query)
+    st.write("Response:", generated_response)
+    st.divider()
+    titles_and_keywords = []
+    st.subheader('Context: \n')
+    for document in compressed_docs:
+        st.write(document.page_content)
+        title = document.metadata.get('title', 'No Title')
+        keywords = document.metadata.get('keywords', [])
+        titles_and_keywords.append({'title': title, 'keywords': keywords})
+    st.subheader('Related Documents: \n')    
+    for item in titles_and_keywords:
+        if not item['title'] == 'No Title':
+            st.write(f"Title: \n{item['title']}")
+            st.write(f"Keywords: \n{', '.join(item['keywords'])}")
+            st.write("\n")  
+            st.divider()
+        
+    follow_up_questions = generate_questions(compressed_docs, 3)
+    questions_list = json.loads(follow_up_questions)
+    for q in questions_list.values():
+        if st.button(q):
+            st.session_state['query'] = q
+            st.experimental_rerun()
+
+def format_docs(docs):
+    """
+    Format documents for display.
+
+    Args:
+        docs (List): List of documents.
+
+    Returns:
+        str: Formatted document content.
+    """
+    return "\n\n".join(doc.page_content for doc in docs)
+
+def generate_questions(context, amount):
+    """
+    Generate follow-up questions based on the context.
+
+    Args:
+        context (str): Context for generating questions.
+        amount (int): Number of questions to generate.
+
+    Returns:
+        str: JSON formatted follow-up questions.
+    """
+    question_type = ["confirmation questions [yes or no]", "factoid-type questions [what, which, when, who, how]",
+                     "list-type questions", "casual questions [why or how]", "hypothetical questions [e.g. what would happen if...]", "questions"]
+    random_type = random.randint(0, len(question_type) - 1)
+
+    prompt = f"""Context: {context}\n\n. Form {question_type[random_type]} in your own words. Answer in this json format {{"question_1": '', ...}} (Exact Amount: {amount}), don't say anything else."""
+
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "assistant"},
+            {"role": "user", "content": prompt},
+        ],
+        temperature=0.7,
+        max_tokens=500,
+        n=2
+    )
+    return response.choices[0].message.content
+
+if __name__ == '__main__':
+    hs, prompt, llm, compression_retriever = initialize_components()
+    display_conversation_interface(compression_retriever, prompt, llm)
--- Hybrid_search.py
+++ Hybrid_search.py
@@ -3,34 +3,75 @@ import os
 from langchain.schema import Document
 from langchain_community.retrievers import BM25Retriever
 from langchain.retrievers import EnsembleRetriever
-from langchain_community.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
 from tqdm.auto import tqdm
+import warnings
+
+# Ignore warnings
+warnings.filterwarnings("ignore")
+
+def suppress_warnings():
+    """
+    A function to suppress DeprecationWarnings.
+    """
+    warnings.warn("deprecated", DeprecationWarning)
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    suppress_warnings()
 
 class HybridSearch:
-    def __init__(self, data_path, faiss_index_path):
+    def __init__(self, data_path):
+        """
+        Initialize the HybridSearch instance.
+
+        Args:
+            data_path (str): Path to the data file.
+        """
         self.data_path = data_path
-        self.faiss_index_path = faiss_index_path
-        os.environ['OPENAI_API_KEY'] = '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'
+        os.environ['OPENAI_API_KEY'] = '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'
         self.embedding = OpenAIEmbeddings()
         self.ensemble_retriever = None
 
     def load_data(self):
+        """
+        Load data from the specified data path.
+
+        Returns:
+            dict: Loaded data.
+        """
         with open(self.data_path, 'r') as file:
             data = json.load(file)
         return data
 
-
-
-
     def initialize_bm25_retriever(self, docs):
+        """
+        Initialize and configure the BM25 retriever.
+
+        Args:
+            docs (list): List of Document instances.
+
+        Returns:
+            BM25Retriever: Initialized BM25Retriever instance.
+        """
         if not all(isinstance(doc, Document) for doc in docs):
             raise ValueError("All items in docs must be Document instances.")
         abstracts = [doc.page_content for doc in docs]
         bm25_retriever = BM25Retriever.from_texts(abstracts, metadatas=[doc.metadata for doc in docs])
-        bm25_retriever.k = 2
+        bm25_retriever.k = 3
         return bm25_retriever
+
     def transform_data_to_documents(self, data):
+        """
+        Transform raw data into Document instances.
+
+        Args:
+            data (dict): Raw data.
+
+        Returns:
+            list: List of Document instances.
+        """
         docs = []
         for doc in data:
             title = doc.get('title', {}).get('full_text', '')
@@ -40,60 +81,60 @@ class HybridSearch:
             docs.append(document)
         return docs
 
+    def process_documents_with_chroma(self, docs):
+        """
+        Process documents with Chroma vector store.
 
+        Args:
+            docs (list): List of Document instances.
 
-    def process_documents_with_faiss(self, docs):
-        if os.path.exists(self.faiss_index_path):
-            print("FAISS index already exists. Loading existing index.")
-            return FAISS.load_local(self.faiss_index_path, self.embedding)
-        
-        # Ensure docs are Document objects
+        Returns:
+            Chroma: Chroma vector store.
+        """
+        persist_directory = './chroma_openai'
+        db3 = Chroma(persist_directory=persist_directory, embedding_function=self.embedding)
         if not all(isinstance(doc, Document) for doc in docs):
             raise ValueError("All items in docs must be Document instances.")
-        abstracts = [doc.page_content for doc in docs]
-        metadata = [doc.metadata for doc in docs]
-        faiss_vectorstore = None
-        batch_size = 1000
-        for i in tqdm(range(0, len(abstracts), batch_size), desc="Processing documents"):
-            batch_texts = abstracts[i:i+batch_size]
-            batch_metadatas = metadata[i:i+batch_size]
-            if faiss_vectorstore is None:
-                faiss_vectorstore = FAISS.from_texts(batch_texts, self.embedding, batch_metadatas)
-            else:
-                faiss_vectorstore.update(batch_texts, self.embedding, batch_metadatas)
-        
-        faiss_vectorstore.save_local(self.faiss_index_path)
-        return faiss_vectorstore
+        chroma_retriever = db3.as_retriever(search_kwargs={'k': 3})
+        return chroma_retriever
 
-    def create_ensemble_retriever(self, bm25_retriever, faiss_vectorstore):
-        faiss_retriever = faiss_vectorstore.as_retriever(search_kwargs={'k': 2})
-        ensemble_retriever = EnsembleRetriever(retrievers=[bm25_retriever, faiss_retriever], weights=[0.5, 0.5])
+    def create_ensemble_retriever(self, bm25_retriever, chroma_retriever):
+        """
+        Create an ensemble retriever.
+
+        Args:
+            bm25_retriever (BM25Retriever): BM25Retriever instance.
+            chroma_retriever (Chroma): Chroma vector store.
+        """
+        ensemble_retriever = EnsembleRetriever(retrievers=[bm25_retriever, chroma_retriever], weights=[0.7, 0.3])
         self.ensemble_retriever = ensemble_retriever
 
     def get_relevant_documents(self, query):
+        """
+        Get relevant documents based on the user query.
+
+        Args:
+            query (str): User's query.
+
+        Returns:
+            list: Formatted relevant documents.
+        """
         results = self.ensemble_retriever.get_relevant_documents(query)
+       
         formatted_results = []
         for document in results:
             doc_info = {
-                'title': document.metadata.get('title', 'No Title'),
+                'title': document.metadata.get('title', document.metadata.get('source/title','No Title')),
                 'keywords': document.metadata.get('keywords', []),
                 'abstract': document.page_content
             }
             formatted_results.append(doc_info)
-        return formatted_results 
+        return formatted_results
 
-if __name__ == "__main__":
-    hs = HybridSearch('E:\\NLPT\\_Q-A-INLPT-WS2023\\Transfromer_project-20240228T221604Z-002\\Transfromer_project\\data\\papers_latest.json', 
-                      'E:\\NLPT\\_Q-A-INLPT-WS2023\\Transfromer_project-20240228T221604Z-002\\Transfromer_project\\db\\faiss_index')
-    data = hs.load_data()
-    docs = hs.transform_data_to_documents(data)
-    print(f"First item type before passing to BM25: {type(docs[0])}")
-    bm25_retriever = hs.initialize_bm25_retriever(docs)
-    
-    faiss_vectorstore = hs.process_documents_with_faiss(docs)
-    hs.create_ensemble_retriever(bm25_retriever, faiss_vectorstore)
-
-    query = 'what is facebook?'
-    ensemble_results = hs.get_relevant_documents(query)  # This will now receive a list
-    for result in ensemble_results:
-        print(result)
+# Example Usage:
+# hs = HybridSearch('./papers_latest.json')
+# data = hs.load_data()
+# docs = hs.transform_data_to_documents(data)
+# bm25_retriever = hs.initialize_bm25_retriever(docs)
+# chroma_vectorstore = hs.process_documents_with_chroma(docs)
+# hs.create_ensemble_retriever(bm25_retriever, chroma_vectorstore)
